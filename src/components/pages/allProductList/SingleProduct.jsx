8630e8bfa412e8ce10c28beeeec6030017dea613
Jatin Devaiya <jatin@kaushalaminc.com>
2023-06-01 14:36:13 +0530

    create user review and rating use with API

--- src/components/pages/allProductList/SingleProduct.jsx
+++ src/components/pages/allProductList/SingleProduct.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Footer from "../footer/Footer";
 import { useParams } from "react-router";
 import axios from "axios";
 
@@ -7,16 +6,20 @@ const SingleProduct = () => {
   const [getProductData, setGetProductData] = useState({});
   const { id } = useParams();
   console.log(id, "id");
+  const [review, setReview] = useState("");
+  const [rating, setRating] = useState(0);
+  const [name, setName] = useState("");
+
+  const [getUserReview, setgetUserReview] = useState([]);
+
   useEffect(() => {
     axios
       .get(`https://fake-e-commerce-api.onrender.com/product/${id}`)
       .then((res) => {
-        // console.log(res.data, "productidapi");
+        console.log(getProductData, "getProductData");
         setGetProductData(res.data);
       });
   }, [id]);
-
-  console.log(getProductData, "getProductData");
 
   const ADD_PRODUCT_TO_CART_API = () => {
     axios
@@ -37,6 +40,41 @@ const SingleProduct = () => {
         [id]
       );
   };
+
+  const ADD_REVIEW_API = () => {
+    axios
+      .post(
+        `https://fake-e-commerce-api.onrender.com/product/reviews/${id}/add`,
+        {
+          name: name,
+          rating: rating,
+          review: review,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        console.log(res.data, "ADD REVIEW,RATING");
+        return res.data;
+      });
+  };
+
+  const GET_USERS_REVIEW = () => {
+    axios
+      .get(`https://fake-e-commerce-api.onrender.com/product/reviews/${id}`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res.data, "GET USERS RIVIEW");
+        setgetUserReview(res.data);
+        return res.data;
+      });
+  };
+
+  useEffect(() => {
+    GET_USERS_REVIEW();
+  }, []);
 
   return (
     <>
@@ -181,7 +219,7 @@ const SingleProduct = () => {
                         aria-controls="ex1-pills-4"
                         aria-selected="false"
                       >
-                        Seller profile
+                        Reviews
                       </a>
                     </li>
                   </ul>
@@ -238,15 +276,108 @@ const SingleProduct = () => {
                       role="tabpanel"
                       aria-labelledby="ex1-tab-4"
                     >
-                      Some other tab content or sample information now <br />
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in reprehenderit in voluptate velit
-                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                      sint occaecat cupidatat non proident, sunt in culpa qui
-                      officia deserunt mollit anim id est laborum.
+                      {/* Add input fields for review, rating, and name */}
+                      <section style={{ backgroundColor: "#e7effd" }}>
+                        <div className="container my-5 py-5 text-dark">
+                          <div className="row d-flex justify-content-center">
+                            <div className="col-md-11 col-lg-9 col-xl-7">
+                              <div className="d-flex flex-start mb-4">
+                                <div className="card w-100">
+                                  <h5 className="text-light bg-dark">
+                                    Please Send Your Review & Reting
+                                  </h5>
+
+                                  <div className="card-body p-4">
+                                    <div className>
+                                      <h5>
+                                        <input
+                                          type="text"
+                                          value={name}
+                                          onChange={(e) =>
+                                            setName(e.target.value)
+                                          }
+                                          placeholder="Enter your name"
+                                        />
+                                      </h5>
+
+                                      <span className="fw-bold">
+                                        Rating : &nbsp;
+                                      </span>
+                                      <select
+                                        className="select"
+                                        value={rating}
+                                        onChange={(e) =>
+                                          setRating(e.target.value)
+                                        }
+                                      >
+                                        <option value={"1"}>One</option>
+                                        <option value={"2"}>Two</option>
+                                        <option value={"3"}>Three</option>
+                                        <option value={"4"}>Four</option>
+                                        <option value={"5"}>Five</option>
+                                      </select>
+                                      <br />
+                                      <p>
+                                        <input
+                                          type="text"
+                                          value={review}
+                                          onChange={(e) =>
+                                            setReview(e.target.value)
+                                          }
+                                          placeholder="Enter your review"
+                                        />
+                                      </p>
+                                      <button
+                                        className="btn btn-primary"
+                                        onClick={ADD_REVIEW_API}
+                                      >
+                                        Add Review
+                                      </button>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+
+                              {/* GET_USERS_REVIEW */}
+
+                              <div className="d-flex flex-start">
+                                <div className="card w-100">
+                                  <h5 className="text-light bg-dark">
+                                    User All Review & Rating
+                                  </h5>
+
+                                  {getUserReview.map((userReview) => {
+                                    return (
+                                      <div className="card-body p-4">
+                                        <div className>
+                                          <h5>
+                                            <span className="text-muted">
+                                              userName :{" "}
+                                            </span>{" "}
+                                            {userReview.name}
+                                          </h5>
+                                          <h6>
+                                            <span className="text-muted">
+                                              productRating :{" "}
+                                            </span>
+                                            {userReview.rating}
+                                          </h6>
+                                          <p>
+                                            <span className="text-muted">
+                                              productReview :{" "}
+                                            </span>
+                                            {userReview.review}
+                                          </p>
+                                        </div>
+                                      </div>
+                                    );
+                                  })}
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </section>
                     </div>
                   </div>
                   {/* Pills content */}
